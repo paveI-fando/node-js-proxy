fd4b34b0aa67a0456768a4ddeb86abdd90edfd29
Fando <p.fando@itransition.com>
2020-02-03 20:26:53 +0300

    change logic of inserting elements

--- app.js
+++ app.js
@@ -13,34 +13,40 @@ var proxy = httpProxy.createProxyServer({
 proxy.on('proxyRes', function (proxyRes, req, res) {
     modifyResponse(res, proxyRes.headers['content-encoding'], function (body) {
             let str = "<div id=\"swagger-editor\"></div>";
-            let index = body.indexOf();
-            let b = "<button onclick=\"showDiv()\">NEW Test</button>";
-            let sc = addScripts() + addStyles() + `
-
-<div id="page-container">
-    <div class="specific-button-container">
-        <button class="specific-button" onclick="showDiv()">New Branch</button>
-        <button class="specific-button">Commit</button>
-        <button class="specific-button">Push</button>
-    </div>
-    <div id="swagger-editor">infa</div>
-</div>
-<div id="abc">
-    <div id="popupContact">
-        <!-- Contact Us Form -->
-        <form action="#" id="form-new-branch" method="post" name="form">
-            <img id="close" src="images/3.png" onclick="hideDiv()">
-            <h2>Contact Us</h2>
-            <hr>
-            <input id="name" name="name" placeholder="BranchName" type="text"/>
-            <a href="javascript:%20checkEmpty()" class="submit-btn">Send</a>
-            <a href="javascript:%20hideDiv()" class="cancel-btn">Cancel</a>
-        </form>
-    </div>
-</div>`;
-            let u = body.slice(0, index) + sc + body.slice(index, body.size);
-            console.log(u);
-        return u;}
+            // let index = body.indexOf();
+            // let b = "<button onclick=\"showDiv()\">NEW Test</button>";
+//             let sc = addScripts() + addStyles() + `
+//
+// <div id="page-container">
+//     <div class="specific-button-container">
+//         <button class="specific-button" onclick="showDiv()">New Branch</button>
+//         <button class="specific-button">Commit</button>
+//         <button class="specific-button">Push</button>
+//     </div>
+//     <div id="swagger-editor">infa</div>
+// </div>
+// <div id="popup-wrapper">
+//     <div id="popup-commit">
+//         <!-- Contact Us Form -->
+//         <form action="#" id="form-new-branch" method="post" name="form">
+//             <img id="close" src="images/3.png" onclick="hideDiv()">
+//             <h2>Contact Us</h2>
+//             <hr>
+//             <input id="commit-msg-input" name="commit-msg" placeholder="BranchName" type="text"/>
+//             <a href="checkEmpty()" class="submit-btn">Send</a>
+//             <a href="hideDiv()" class="cancel-btn">Cancel</a>
+//         </form>
+//     </div>
+// </div>`;
+//             let u = body.slice(0, index) + sc + body.slice(index, body.size);
+//             console.log(u);
+            if (req.url === "/swagger-editor/") {
+                body = body.slice(0, body.indexOf('<body>') + 6) + addScripts() + body.slice(body.indexOf('<body>') + 6, body.length);
+                // body = body.slice(0, body.lastIndexOf('</style>') + 8) + addStyles() + body.slice(body.lastIndexOf('</style>') + 8, body.length);
+                // console.log(body);
+            }
+            return body;
+        }
     );
 });
 
--- git.js
+++ git.js
@@ -12,28 +12,80 @@ exports.branchCommand = function () {
 
 exports.addScripts = function () {
     return `<script> 
-function checkEmpty() {
-    if (document.getElementById('name').value === "" || document.getElementById('email').value === "" || document.getElementById('msg').value === "") {
-        alert("Fill All Fields !");
-    } else {
-        document.getElementById('form').submit();
-        alert("Form Submitted Successfully...");
-    }
-}
+    window.addEventListener('load',  function() {
+      let el = document.querySelector('div#swagger-editor');
+      let wrapper = document.createElement('div');
+      wrapper.setAttribute('id', 'page-container');
+      let buttonBox = document.createElement('div');
+      buttonBox.setAttribute('id', 'specific-button-container');
+      buttonBox.innerHTML = 
+      \`
+        <button id="new-branch-btn" class="specific-button">New Branch</button>
+        <button id="commit-btn" class="specific-button">Commit</button>
+        <button id="push-btn" class="specific-button">Push</button>\`;
+      let popupWrapper = document.createElement('div');
+      popupWrapper.style = \` 
+            width:100%;
+            height:100%;
+            top:0;
+            left:0;
+            display:none;
+            position:absolute;
+            overflow:auto;
+            z-index: 6;\`;
+      popupWrapper.setAttribute('id', 'popup-wrapper');
+      popupWrapper.innerHTML = 
+      \`
+      ${getPopUp()};
+      \`;
+      el.parentNode.insertBefore(wrapper, el);
+      wrapper.appendChild(buttonBox);  
+      wrapper.appendChild(el);
+      document.body.appendChild(popupWrapper);
+      let head = document.head || document.getElementsByTagName('head')[0];
+      let style = document.createElement('style');
+      head.appendChild(style);
+      style.appendChild(document.createTextNode(\`${addStyles()}\`));
+      document.getElementById("new-branch-btn").addEventListener("click", showDiv);
+    });
+    // if (document.getElementById('name').value === "" || document.getElementById('email').value === "" || document.getElementById('msg').value === "") {
+    //     alert("Fill All Fields !");
+    // } else {
+    //     document.getElementById('form').submit();
+    //     alert("Form Submitted Successfully...");
+    // }
+    
+// }
+//
+
 
 function showDiv() {
-    document.getElementById('abc').style.display = "block";
+    document.getElementById('popup-wrapper').style.display = "block";
 }
 
 function hideDiv(){
-    document.getElementById('abc').style.display = "none";
+    document.getElementById('popup-wrapper').style.display = "none";
 }
 </script>`
 };
 
-exports.addStyles = function () {
+function getPopUp() {
+ return `
+    <div id="popup-commit">
+        <!-- Contact Us Form -->
+        <form action="#" id="form-new-branch" method="post" name="form">
+            <img id="close" src="images/3.png" onclick="hideDiv()">
+            <h2>Contact Us</h2>
+            <hr>
+            <input id="commit-msg-input" name="commit-msg" placeholder="BranchName" type="text"/>
+            <a href="javascript:%20checkEmpty()" class="submit-btn">Send</a>
+            <a href="javascript:%20hideDiv()" class="cancel-btn">Cancel</a>
+        </form>
+    </div>
+ `}
+
+function addStyles () {
     return `
-<style>
         .specific-button-container {
             width: 50%;
             padding: 10px;
@@ -65,7 +117,7 @@ exports.addStyles = function () {
             position:relative;
             top:1px;
         }
-        #abc {
+        #popup-wrapper {
             width:100%;
             height:100%;
             top:0;
@@ -122,17 +174,17 @@ exports.addStyles = function () {
             font-family:sans-serif;
         }
         #name {
-            background-image:url(../images/name.jpg);
+            /*background-image:url(../images/name.jpg);*/
             background-repeat:no-repeat;
             background-position:5px 7px
         }
         #email {
-            background-image:url(../images/email.png);
+            /*background-image:url(../images/email.png);*/
             background-repeat:no-repeat;
             background-position:5px 7px
         }
         textarea {
-            background-image:url(../images/msg.png);
+            /*background-image:url(../images/msg.png);*/
             background-repeat:no-repeat;
             background-position:5px 7px;
             width:82%;
@@ -182,5 +234,5 @@ exports.addStyles = function () {
             font-size:18px;
             cursor:pointer
         }
-</style>`
-};
+`
+}
